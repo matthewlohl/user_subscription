62ab72422087ea8df1c45b291cfa1c74f0fd6d7e
Camila Pessoa <caape13@hotmail.com>
2022-10-18 12:23:24 +0100

    implement first routes

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask (__name__)
+CORS(app)
 
+@app.route('/')
+def hello():
+    return 'Welcome to our API!'
+
+@app.route('/users', methods= ["GET", "POST", "DELETE"]):
+def user():
+    if 
+
+if __name__=="__main__":
+    app.run(debug=True)
